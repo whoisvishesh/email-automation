79c8fec2689298766485d9a18b8c475a5f66f750
Vishesh chintan <vishesh.chintan1206@gmail.com>
2023-05-06 16:09:18 +0530

    Final file with the cron job files added

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 from datetime import date
 import pandas as pd
-from send_email import send_email, template_1
+from send_email import send_email
 
-sheet_id = "1rOQW0p4p1v2OC-UL-T2VpXCTeNLwWCM8I5n0mx_iEJ8"
-sheet_name = "Sheet1"
+sheet_id = '1rOQW0p4p1v2OC-UL-T2VpXCTeNLwWCM8I5n0mx_iEJ8'
+sheet_name = 'Sheet1'
 URL = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
 
 def load_df(url):
     parse_dates = ["due_date", "reminder_date"]
-    df = pd.read_csv(url, parse_dates=parse_dates)
+    df = pd.read_csv(url, parse_dates=parse_dates, dayfirst=True)
     return df
 
 
 def send_emails_using_data(df):
     present = date.today()
+    #print(type(present))
     email_counter = 0
     for _, row in df.iterrows():
-        if(present >= row["reminder_date"].date()) and (row["has_paid"] == "no"):
+        #print(type(row['reminder_date']))
+        if (present >= pd.to_datetime(row['reminder_date']).date()) and (row['has_paid'] == False):
             send_email(
-                template_1,
+                subject=f'Payment Reminder: Due Date is near, Invoice: {row["invoice_no"]}',
                 name=row["name"],
                 receiver_email=row["email"],
-                due_date=row["due_date"].strftime("%d, %b %Y"),
+                due_date=pd.to_datetime(row['reminder_date']).strftime("%d, %b %Y"),
                 invoice_no=row["invoice_no"],
-                amount=row["amount"]
+                amount=row["amount"],
             )
 
             email_counter += 1
--- send_email.py
+++ send_email.py
@@ -1,8 +1,7 @@
-import os
 import smtplib
 from email.message import EmailMessage
 from email.utils import formataddr
-from pathlib import Path
+from datetime import date
 
 PORT = 587
 EMAIL_SERVER = "smtp.gmail.com"
@@ -12,54 +11,77 @@ with open('.env', 'r') as f:
     sender_email = f.readline().strip()
     password_email = f.readline().strip()
 
-# Define email templates
-template_1 = {
-    'subject': 'Payment Reminder',
-    'body': '''\
-        Hi {name},
-        I hope this email finds you well.
 
-        This is just a friendly reminder that payment for the invoice {invoice_no} is due on {due_date}. 
-        The outstanding amount is Rs. {amount}/-.
-
-        Please let me know if there are any questions or concerns regarding this invoice.
-
-        Best regards,
-        xyz firm
-    '''
-}
-
-template_2 = {
-    'subject': 'Final Payment Reminder',
-    'body': '''\
-        Hi {name},
-        I hope you are doing well.
-
-        This is a final reminder that payment for the invoice {invoice_no} is due today. 
-        The outstanding amount is Rs. {amount}/-. 
-        If payment is not received today, we will need to take further action.
-
-        Please let me know if there are any questions or concerns regarding this invoice.
-
-        Best regards,
-        YOUR NAME
-    '''
-}
-
-
-def send_email(template, receiver_email, name, due_date, invoice_no, amount):
+def send_email(subject, receiver_email, name, due_date, invoice_no, amount):
     # Create the base text message.
     msg = EmailMessage()
-    msg["Subject"] = template['subject']
+    msg["Subject"] = subject
     msg["From"] = formataddr(("Engineering College Ajmer", f"{sender_email}"))
     msg["To"] = receiver_email
     msg["BCC"] = sender_email
 
     # Format the email body using the specified template
-    body = template['body'].format(name=name, due_date=due_date, invoice_no=invoice_no, amount=amount)
+    # body = template['body'].format(name=name, due_date=due_date, invoice_no=invoice_no, amount=amount)
 
-    msg.set_content(body)
+    # msg.set_content(body)
+    msg.set_content(
+        f'''\
+        Hi <strong>{name}</strong>,
+        I hope you are doing well.
 
+        This is a final reminder that payment for the invoice <strong>{invoice_no}</strong> is due on <strong>{due_date}</strong>.
+        The outstanding amount is Rs. <strong>{amount}/-</strong>.
+
+        If payment is not received by the due date, we will need to take further action.
+
+        Please let me know if there are any questions or concerns regarding this invoice.
+
+        Best regards,
+        Whoisvishesh
+        '''
+
+    # template_2 = {
+    #   'subject': 'Final Payment Reminder: Last day',
+    #  'body': '''\
+    # Hi <b>{name}</b>,
+
+    # I hope you are doing well.
+
+    # This is a final reminder that payment for the invoice <b>{invoice_no}</b> is due <b>{due_date}</b>.
+    # The outstanding amount is Rs. <b>{amount}/-</b>.
+
+    # If payment is not received today, we will need to take further action.
+
+    # Please let me know if there are any questions or concerns regarding this invoice.
+
+    # Best regards,
+    # Whoisvishesh
+    #       '''
+    # }'''
+
+    )
+
+    msg.add_alternative(
+        f'''\
+        <html>
+            <body>
+                <p>Hi <strong>{name}</strong>,</p>
+                <p>I hope you are doing well.</p>
+
+                <p>This is a final reminder that payment for the invoice <strong>{invoice_no}</strong> is due on <strong>{due_date}</strong>.</p>
+                <p>The outstanding amount is Rs. <strong>{amount}/-</strong>.</p>
+        
+                <p>If payment is not received by the due date, we will need to take further action.</p>
+        
+                <p>Please let me know if there are any questions or concerns regarding this invoice.</p>
+        
+                <p>Best regards,</p>
+                <p>Whoisvishesh</p>
+            </body>
+        </html>
+        ''',
+        subtype="html",
+    )
     with smtplib.SMTP(EMAIL_SERVER, PORT) as server:
         server.starttls()
         server.login(sender_email, password_email)
@@ -67,22 +89,11 @@ def send_email(template, receiver_email, name, due_date, invoice_no, amount):
 
 
 if __name__ == "__main__":
-    # Send the first email reminder
     send_email(
-        template_1,
+        subject="Payment Reminder: Due Date is near.....",
         name="Vishesh Chintan",
         receiver_email="whoisvishesh@gmail.com",
         due_date="25th APRIL 2023",
-        invoice_no="INV-21-12-009",
-        amount="5",
-    )
-
-    # Send the final email reminder
-    send_email(
-        template_2,
-        name="Vishesh Chintan",
-        receiver_email="whoisvishesh@gmail.com",
-        due_date="30th APRIL 2023",
         invoice_no="INV-21-12-009",
         amount="5",
     )
